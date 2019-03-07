c944bf9a77cb516beba4cc5a77eb71a119bfea6e
ZohanHo <serduk.evgeniy@gmail.com>
2019-03-08 01:03:13 +0200

    fayno v 1.1

--- start/views.py
+++ start/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def start(request):
     return render(request, 'start_page/start_page.html')
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
